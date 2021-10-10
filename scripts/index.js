24d94c99023ddf31f4a97a9932b74f73a51b5799
Daniel Sotnikov <dsotnikov3@gmail.com>
2021-10-11 00:17:03 +0300

    исправить пометки ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,10 @@
-let like = document.querySelectorAll('.user-gallery__like-b');
-
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
 const editButton = document.querySelector('.user__add-info');
 const form = document.querySelector('.popup__form');
 
-const nameField = document.querySelector('.popup__user-name')
-const infoField = document.querySelector('.popup__user-info')
+const nameField = document.querySelector('.popup__input_element_user-name')
+const infoField = document.querySelector('.popup__input_element_user-info')
 
 const userName = document.querySelector('.user__name')
 const userInfo = document.querySelector('.user__info')
@@ -15,26 +13,20 @@ const userInfo = document.querySelector('.user__info')
 
 
 function openPopup() {
-  popup.classList.add('popup_isOpen')
+  popup.classList.add('popup_is-open');
+  nameField.value = userName.textContent;
+  infoField.value = userInfo.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove('popup_isOpen')
+  popup.classList.remove('popup_is-open');
 }
 
 
-editButton.addEventListener('click', openPopup)
+editButton.addEventListener('click', openPopup);
 
-popupCloseButton.addEventListener('click', closePopup)
+popupCloseButton.addEventListener('click', closePopup);
 
-function popupClickHandler(event) {
-
-  if (event.target.classList.contains('popup')) {
-    closePopup()
-  }
-}
-
-popup.addEventListener('mouseup', popupClickHandler)
 
 function submitForm(event) {
   event.preventDefault()
